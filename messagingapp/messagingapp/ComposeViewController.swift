ea5c2aeb7608a98fb112242453dbd2d6e35a6fa0
Shivang Ranjan <shivang12134@gmail.com>
2019-01-13 21:39:02 +0530

    Building the User Interface

---
+++ messagingapp/messagingapp/ComposeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ComposeViewController.swift
+//  messagingapp
+//
+//  Created by Shivang Ranjan on 13/01/19.
+//  Copyright © 2019 Shivang Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addPost(_ sender: Any) {
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- messagingapp/messagingapp/ViewController.swift
+++ messagingapp/messagingapp/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var postData = ["Message1", "Message2", "Message3"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        
+        return cell!
+    }
+    
 }
 
